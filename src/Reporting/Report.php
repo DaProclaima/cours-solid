00ad75b39f549f0eca351721a2bdb476595f4408
Sebastien NOBOUR <sebastien.nobour@valeo.com>
2019-11-14 14:03:17 +0100

    refactor(./src/Reporting/Format/Report.php): Applied the SRP to this class. formatToHTML() and formatToJson() got moved to new classes HtmlFormatter.php and JsonFormatter.php

--- src/Reporting/Report.php
+++ src/Reporting/Report.php
@@ -20,11 +20,6 @@ class Report
     protected $data;
 
 
-// In future a report might contain also an author. So its structure might change as well as its constructor. 1st reason why we should modify this class
-
-
-
-
     /**
      * Constructeur qui reçoit la date et le titre du rapport
      *
@@ -36,41 +31,6 @@ class Report
         $this->date  = $date;
         $this->title = $title;
         $this->data = $data;
-    }
-
-    /**
-     * Retourne le rapport formatté en HTML
-     *
-     * @return string
-     */
-    public function formatToHTML(): string
-    {
-        $data = "";
-
-        foreach ($this->data as $value) {
-            $data .= "<li>$value</li>";
-        }
-
-        return "
-            <h2>$this->title</h2>
-            <em>Date : $this->date</em>
-            <h4>Données : </h4>
-            <ul>
-                $data
-            </ul>
-        ";
-    }
-
-// In future the html template might change as switching from h2 to h1 etc. 2nd reason why to change this class 
-
-    /**
-     * Retourne le rapport formatté en JSON
-     *
-     * @return string
-     */
-    public function formatToJSON(): string
-    {
-        return json_encode($this->getContents());
     }
 
     /**
@@ -87,6 +47,6 @@ class Report
     }
 }
 
-// In future the json template might change as adding the new attributes we give to a report structure. 3rd reason why to change this class.
-// We don t respect the SRP since this class has 3 responsibilities: represent a report, format it to html and json. So there should be 3 classes
+// The SRP is respected now. The only reason why we might change the class is if the representation of a report has an author or something else in plus or less.
+
 
