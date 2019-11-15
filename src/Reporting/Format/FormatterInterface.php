4248b4a1b01fb2bc25154fa177479943290422cf
Sebastien NOBOUR <sebastien.nobour@valeo.com>
2019-11-15 11:44:09 +0100

    refactor(JsonFormatter.php): Refactored the class to use the new interface FormatterInterface.php with its 2 methods deserialize and format + wrote back the parameter name in deserialize method as  instead of

--- src/Reporting/Format/FormatterInterface.php
+++ src/Reporting/Format/FormatterInterface.php
@@ -7,5 +7,5 @@ use App\Reporting\Report;
 // A contract between this interface and the classes using it force them to use its methods
 interface FormatterInterface {
     public function format(Report $report) : string;
-    public function deserialize(string $string ) : Report;
+    public function deserialize(string $str ) : Report;
 }
--- src/Reporting/Format/JsonFormatter.php
+++ src/Reporting/Format/JsonFormatter.php
@@ -4,11 +4,21 @@ namespace App\Reporting\Format;
 
 use App\Reporting\Report;
 
-class jsonFormatter {
-    public function formatToJson(Report $report) {
+class jsonFormatter implements FormatterInterface
+ {
+    public function format(Report $report) : string
+     {
 
         return json_encode($report->getContents());
 
+    }
+
+    public function deserialize(string $str): Report
+    {
+        // { "title": "Mon titre", "date": "2019-02-02", "data": [5,6]} // str
+        $contents = json_decode($str);
+
+        return new Report($contents['date'], $contents['title'], $contents['data']);
     }
 }
 // The SRP is respected now. The only reason why we might change the class is if I write the Json differently
