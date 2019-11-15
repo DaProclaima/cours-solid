21c0df15697a0b1561a9dd57619873c75529333b
Sebastien NOBOUR <sebastien.nobour@valeo.com>
2019-11-15 12:43:43 +0100

    docs(HtmlFormatter): why an interface should have a few methods and be segregated.
    change public function deserialize(string ): Report
    
    It is useless in HTML. So the Interface has a method in too much

--- src/Reporting/Format/HtmlFormatter.php
+++ src/Reporting/Format/HtmlFormatter.php
@@ -3,9 +3,12 @@
 namespace App\Reporting\Format;
 
 use App\Reporting\Report;
+use Exception;
 
-class HtmlFormatter {
-    public function formatToHtml(Report $report){
+class HtmlFormatter implements FormatterInterface
+ {
+    public function format(Report $report): string
+    {
     
         $contents = $report->getContents();
 
@@ -23,6 +26,13 @@ class HtmlFormatter {
                 $data
             </ul>
         ";
+    }
+
+    public function deserialize(string $str): Report
+    {
+        throw new Exception("Il est impossible de désérialiser du HTML");
+        return new Report("", "", []);
+        // Faire ça c'est débile
     }
 }
 // Do I have several reason to change this class? No, the SRP is respected now. The only reason why we might change the class is if I write the Json differently
